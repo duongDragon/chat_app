617f93236083e3f0a12127f8aa222557b492f97c
duongDragon <caohaiduong2002@gmail.com>
2024-03-19 21:23:50 +0700

    chat message

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widget/chat_messages.dart';
+import 'package:chat_app/widget/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,20 +9,24 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutterChat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: const Icon(Icons.exit_to_app),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
+        body: Column(
+          children: const [
+            Expanded(
+              child: ChatMessage(),
+            ),
+            NewMessages(),
+          ],
+        ));
   }
 }
